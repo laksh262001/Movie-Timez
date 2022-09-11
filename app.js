c5da14eae387e349f006dd43b8db8c58a12af97e
Lakshmi T S <t.s.lakshmi2@gmail.com>
2022-09-11 19:23:05 +0530

    Modified all files

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const _ = require("lodash");
+
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+const welcome = "Welcome to Movie Timez. Select your favourite movie and enjoy watching!";
+
+
+app.get("/", function(req, res){
+    res.render("home", {lines: welcome,
+    });
+  });
 
 app.get('/', function(req,res){
     res.send("Hi Pushpak and Lakshmi");
